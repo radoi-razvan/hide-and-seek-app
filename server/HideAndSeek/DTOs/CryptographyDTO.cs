8f0cd5395964e551a1ed3b0db0d11122e16d4660
radoi-razvan <r4zzzr4zzz@gmail.com>
2022-03-10 23:27:50 +0200

    Create CryptographyDTO.cs

---
+++ server/HideAndSeek/DTOs/CryptographyDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace HideAndSeek.DTOs
+{
+    public class CryptographyDTO
+    {
+        public IFormFile File { get; set; } = null!;
+        public string Key { get; set; } = null!;
+        public string Operation { get; set; } = null!;
+        public bool Crc { get; set; }
+    }
+}
